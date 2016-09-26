7163d18a948516a1e797cfa418255fe37617cf09
Beta <betajason1109@gmail.com>
2016-09-25 23:24:09 -0500

    Added some code
    Still planing to add more tho

--- bot.dart
+++ bot.dart
@@ -1,1 +1,19 @@
+import 'package:discord_dart/discord.dart' as discord; //Do not change this.
 
+main() {
+  var bot = new discord.Client("YOUR-BOT-TOKEN");
+  var prefix = "YOUR-BOT-PREFIX"//Do not use ! please
+  
+  bot.on('ready', (e) { //e is telling the event.
+    print("Bot online and ready!"); //Telling you that the bot is online and ready to use.
+  });
+
+  bot.on('message', (e) { //e is telling the event
+    var m = e.message; //You can change the m to msg or message or whatever you like.
+    //Don't do something big.
+
+    if (m.content.startsWith((prefix + "ping") {
+      m.channel.sendMessage("Pong!");
+    }
+  });
+}
