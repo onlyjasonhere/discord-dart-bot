9009826383fdd3a6e3e7c1aadd2ec1655c67b3dc
Zachary Vacura <zvacura@outlook.com>
2016-09-28 17:15:19 -0500

    Some fixes

--- bot.dart
+++ bot.dart
@@ -1,9 +1,11 @@
-import 'package:discord_dart/discord.dart' as discord; //Do not change this.
+import 'dart:io';
+import 'dart:convert';
+import 'package:discord/discord.dart' as discord; //Do not change this.
 
-main() {
-  var bot = new discord.Client("YOUR-BOT-TOKEN");
-  var prefix = "YOUR-BOT-PREFIX"//Do not use ! please
-  
+main() async {
+  var config = JSON.decode(await new File('config.json').readAsString());
+  var bot = new discord.Client(config['token']);
+
   bot.on('ready', (e) { //e is telling the event.
     print("Bot online and ready!"); //Telling you that the bot is online and ready to use.
   });
@@ -13,8 +15,8 @@ main() {
     //Don't do something big.
     print("User: " + m.author.username + " Guild Name: " + m.guild.name + " Message: " + m.content);
     //Logging system.
-    
-    if (m.content.startsWith((prefix + "ping") {
+
+    if (m.content.startsWith(config['prefix'] + "ping")) {
       m.channel.sendMessage("Pong!");
     }
   });
